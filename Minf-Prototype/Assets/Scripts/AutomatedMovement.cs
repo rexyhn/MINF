0b450393d7b41e6728676b633770d848d1c0caaf
rexyhn <rexyhn3@googlemail.com>
2018-05-29 14:23:51 +0200

    Added Prototype

---
+++ Minf-Prototype/Assets/Scripts/AutomatedMovement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class AutomatedMovement : MonoBehaviour
+{
+    public LayerMask enemyMask;
+    public float speed = 1;
+    Rigidbody2D myBody;
+    Transform myTrans;
+    float myWidth, myHeight;
+
+    void Start()
+    {
+        myTrans = this.transform;
+        myBody = this.GetComponent<Rigidbody2D>();
+        SpriteRenderer mySprite = this.GetComponent<SpriteRenderer>();
+        myWidth = mySprite.bounds.extents.x;
+        myHeight = mySprite.bounds.extents.y;
+    }
+
+    void FixedUpdate()
+    {
+       
+
+    
+        Vector2 lineCastPos = myTrans.position.toVector2() - myTrans.right.toVector2() * myWidth + Vector2.up * myHeight;
+        
+        Debug.DrawLine(lineCastPos, lineCastPos + Vector2.down*0.8f,Color.red,1f);
+     
+        bool isGrounded = Physics2D.Linecast(lineCastPos, lineCastPos + Vector2.down*0.8f, enemyMask);
+      
+        Debug.DrawLine(lineCastPos, lineCastPos - myTrans.right.toVector2() * .05f);
+        bool isBlocked = Physics2D.Linecast(lineCastPos, lineCastPos - myTrans.right.toVector2() * .05f, enemyMask);
+
+        
+        if (!isGrounded || isBlocked)
+        {
+            Vector3 currRot = myTrans.eulerAngles;
+            currRot.y += 180;
+            myTrans.eulerAngles = currRot;
+        }
+        
+
+        
+        
+        Vector2 myVel = myBody.velocity;
+        myVel.x = -myTrans.right.x * speed;
+        myBody.velocity = myVel;
+        
+    }
+   
+}
---
+++ Minf-Prototype/Assets/Scripts/PlatformMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMovement : MonoBehaviour {
+
+    private Vector3 posA;
+    private Vector3 posB;
+    private Vector3 nexPos;
+    [SerializeField]
+    private float platformSpeed;
+    [SerializeField]
+    private Transform childTransform;
+    [SerializeField]
+    private Transform transformB;
+	// Use this for initialization
+	void Start () {
+        posA = childTransform.localPosition;
+        posB = transformB.localPosition;
+        nexPos = posB;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Move();
+	}
+    private void Move()
+    {
+        childTransform.localPosition = Vector3.MoveTowards(childTransform.localPosition, nexPos, platformSpeed * Time.deltaTime);
+        if (Vector3.Distance(childTransform.localPosition, nexPos) <= 0.001) {
+            changeDestination();
+        }
+    }
+    private void changeDestination() {
+        nexPos = nexPos != posA ? posA : posB;
+    }
+}
